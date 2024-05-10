bfa226429ee170de1d83ae4c9cf0739995eb8b80
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-05-10 17:31:00 +0200

    Likes change color when pressed.

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,5 @@
 /*This part deals with the aside*/
-var posts = document.querySelectorAll("article");
+var posts = document.querySelectorAll(".open-focus");
 var postFocus = document.querySelector("aside");
 var closeFocus = document.querySelector(".close-focus");
 
@@ -11,4 +11,16 @@ posts.forEach((post) => {
 
 closeFocus.addEventListener("click", () => {
   postFocus.style.display = postFocus.style.display === "none" ? "grid" : "none";
+});
+
+/*This part deals with the heart button*/
+var heartFull = document.querySelectorAll(".fa-regular.fa-heart");
+var heartEmpty = document.querySelectorAll(".fa-solid.fa-heart");
+
+heartFull.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    console.log("Mi hai cliccato");
+    heart.classList.toggle('fa-solid');
+    heart.classList.toggle('fa-regular');
+  })
 });
