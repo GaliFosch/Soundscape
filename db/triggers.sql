72543ad80d90b1e39ef28e7ad6b40e5daa08c034
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-06-13 10:20:48 +0200

    added first trigger

--- db/triggers.sql
+++ db/triggers.sql
@@ -1,9 +1,33 @@
-delimiter //
+Drop trigger after_post_insert;
+DELIMITER //
 
-create trigger new_post_not_check after insert on post
-for each row
-begin
-	for each (select username from users )
-end;//
+CREATE TRIGGER after_post_insert
+AFTER INSERT ON post
+FOR EACH ROW
+BEGIN
+    DECLARE follower VARCHAR(30);
+    DECLARE done INT DEFAULT FALSE;
 
-delimiter ;
+    DECLARE follower_cursor CURSOR FOR
+        SELECT follow.follower 
+        FROM follow 
+        WHERE following = NEW.Username;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN follower_cursor;
+
+    fetch_follower: LOOP
+        FETCH follower_cursor INTO follower;
+        IF done THEN
+            LEAVE fetch_follower;
+        END IF;
+		INSERT INTO notification (Receiver, Type, NotificationTimestamp, TriggeringUser, PostID) 
+		VALUES (follower, 'Post', current_timestamp(), NEW.Username, NEW.PostID);
+    END LOOP fetch_follower;
+
+    CLOSE follower_cursor;
+
+END //
+
+DELIMITER ;
