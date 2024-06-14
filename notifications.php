0a89e15445cb79b065488bf190e7e035343691c1
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-06-14 17:28:42 +0200

    created base for the notification page

---
+++ notifications.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("bootstrap.php");
+
+$template["title"] = "Soundscape - Notifications";
+$template["stylesheets"] = ["base.css", "notification.css"];
+$template["content"] = "template/notification_templ.php";
+
+require("template/base.php");
