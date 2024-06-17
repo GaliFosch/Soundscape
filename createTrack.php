7a24cc5a266051a0c5b6a85747f7c81112db1529
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-06-17 18:41:51 +0200

    created base for track publishing

---
+++ createTrack.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("bootstrap.php");
+
+$template["title"] = "Soundscape - Create Track";
+$template["stylesheets"] = ["base.css", "createTrack.css"];
+$template["content"] = "template/create_track.php";
+
+require("template/base.php");
