e72789aa7a5b7026117e4d026b1db7e6994e5100
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-06-18 12:47:00 +0200

    added chenging follower count to profile page

--- js/follow.js
+++ js/follow.js
@@ -8,6 +8,7 @@ function followProcedure(follow){
             if(req.responseText === "true"){
                 follow.classList.remove("fa-user-plus")
                 follow.classList.add("fa-user-check")
+                addToFollowerCount();
             }
         }
         req.open("GET", "process_follow.php?target=" + target)
@@ -17,11 +18,26 @@ function followProcedure(follow){
             if(req.responseText === "true"){
                 follow.classList.remove("fa-user-check")
                 follow.classList.add("fa-user-plus")
+                remFromFollowerCount();
             }
         }
         req.open("GET", "process_unfollow.php?target=" + target)
         req.send()
     }
+}
+
+function addToFollowerCount(){
+    let followerCount = document.getElementById("followerCount");
+    let currentCount = parseInt(followerCount.textContent, 10);
+    currentCount += 1;
+    followerCount.textContent = currentCount;
+}
+
+function remFromFollowerCount(){
+    let followerCount = document.getElementById("followerCount");
+    let currentCount = parseInt(followerCount.textContent, 10);
+    currentCount -= 1;
+    followerCount.textContent = currentCount;
 }
 
 let follow = document.getElementById("follow")
