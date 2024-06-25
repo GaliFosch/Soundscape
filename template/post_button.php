1af830e6c61adef3b887eb524b44c17ee5956da2
Ginevra Bartolini <ginevra.bartolini@studio.unibo.it>
2024-06-25 16:13:05 +0200

    "

--- template/post_button.php
+++ template/post_button.php
@@ -1,5 +1,5 @@
 <?php if(isset($_SESSION['username'])): ?>
-    <a href="post_creation.php" class=""post-button-a>
+    <a href="post_creation.php" class="post-button-a">
         <button id="post-button" type="button">
             <em class="fa-solid fa-pencil post-creation"></em>
         </button>
