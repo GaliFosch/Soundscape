6df767b007fd36f9defa19bd6b0b16419e04199c
Elena Boschetti <elena.boschetti@studio.unibo.it>
2024-06-28 10:27:23 +0200

    restyle playlist box in home feed posts and post creation page, remove scroll event in home feed

--- template/create_post.php
+++ template/create_post.php
@@ -47,38 +47,29 @@
     <p class="remove">Remove track</p>
     </section>
     <?php elseif (isset($template["playlist"])): ?>
-        <section class="playlist-box">
-            <section class="inner-playlist-box">
-                <?php if (isset($template["playlist"]['playlist']["CoverImage"])): ?>
-                    <img class="playlist" src="<?php echo $template["playlist"]['playlist']["CoverImage"]; ?>" alt="Song cover image"/>
+        <section class="playlistSection">
+            <a href="playlist.php?id=<?php echo $template["playlist"]["PlaylistID"];?>">
+                <?php if(empty($template["playlist"]["CoverImage"])): ?>
+                    <img class="picture" src="images/placeholder-image.jpg" alt="User profile image"/>
                 <?php else: ?>
-                    <img class="playlist" src="images/placeholder-image.jpg" alt="Song cover image"/>
-                <?php endif; ?>
-                <!--Inner section delle info della music-->
-                <section class="music-info">
-                    <header><strong><?php echo $template["playlist"]['playlist']["Name"]; ?></strong></header>
-                    <p><?php echo $template["playlist"]['playlist']["Creator"]; ?></p>
-                    <p><?php echo $template["playlist"]['playlist']["isAlbum"]==1 ? "Album" : "Playlist"; ?></p>
-                </section>
-                <section class="tracklist-section">
-                    <ol class="tracklist">
-                        <?php foreach ($template["playlist"]["songs"] as $track): ?>
-                            <li class="single-track">
-                                <?php if ($track["CoverImage"] != null): ?>
-                                    <img class="song" src="<?php echo $track["CoverImage"]; ?>" alt="Song cover image"/>
-                                <?php else: ?>
-                                    <img class="song" src="images/placeholder-image.jpg" alt="Song cover image"/>
-                                <?php endif; ?>
-                                <!--Inner section delle info della music-->
-                                <section class="music-info">
-                                    <header><strong><?php echo $track["Name"]; ?></strong></header>
-                                    <p><?php echo $track["Creator"]; ?></p>
-                                </section>
-                            </li>
-                        <?php endforeach; ?>
-                    </ol>
-                </section>
-            </section>
+                    <img class="picture" src="<?php echo $template["playlist"]["CoverImage"]; ?>" alt="User profile image"/>
+                <?php endif;?>
+            </a>
+            <div class="playlist-info">
+                <a href="playlist.php?id=<?php echo $template["playlist"]["PlaylistID"];?>">
+                    <h2 class="playlist-name"><?php echo $template["playlist"]["Name"]; ?></h2>
+                </a>
+                <p class="author"><a href="profile.php?profile=<?php echo $template["playlist"]["Creator"]; ?>"><?php echo $template["playlist"]["Creator"]; ?></a></p>
+                <p>
+                    <?php
+                    if($template["playlist"]["isAlbum"] === "1"){
+                        echo "Album";
+                    }else{
+                        echo "Playlist";
+                    }
+                    ?>
+                </p>
+            </div>
         </section>
         <section class="options">
             <p class="remove">Remove track</p>
@@ -92,7 +83,7 @@
         <textarea class="caption" name="caption" id="write-caption" placeholder="Write here your post" rows="23" wrap="hard" required></textarea>
         <input type="hidden" 
             name="<?php echo $template['type']; ?>"
-            value="<?php if ($template["type"] == 'track'): ?><?php echo $template['track']['TrackID']; ?><?php elseif ($template["type"] == 'playlist'): ?><?php echo $template['playlist']['playlist']['PlaylistID']; ?><?php endif; ?>"></input>
+            value="<?php if ($template["type"] == 'track'): ?><?php echo $template['track']['TrackID']; ?><?php elseif ($template["type"] == 'playlist'): ?><?php echo $template['playlist']['PlaylistID']; ?><?php endif; ?>"></input>
         <label for="images">Add some images:</label>
         <input type="file" id="images" name="images[]" accept="image/jpg, image/jpeg, image/png" multiple>
         <input type="submit" value="Post"/>
