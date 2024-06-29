c39c647c34436d3770ba9e2496e0237b16a12fa2
Elena Boschetti <elena.boschetti@studio.unibo.it>
2024-06-29 13:28:37 +0200

    add adjustments for validation and accessibility, add missing unset of session info

--- logout.php
+++ logout.php
@@ -2,6 +2,29 @@
 
 require_once("bootstrap.php");
 
+// Discard session info related to login
 unset($_SESSION["username"]);
 unset($_SESSION["loginString"]);
+
+// Discard session info related to the music player
+if (isset($_SESSION["loaded_track_id"])) {
+    unset($_SESSION["loaded_track_id"]);
+}
+if (isset($_SESSION["loaded_playlist_id"])) {
+    unset($_SESSION["loaded_playlist_id"]);
+}
+if (isset($_SESSION["shuffle"])) {
+    unset($_SESSION["shuffle"]);
+}
+if (isset($_SESSION["tracklist"])) {
+    unset($_SESSION["tracklist"]);
+}
+if (isset($_SESSION["current_tracklist_index"])) {
+    unset($_SESSION["current_tracklist_index"]);
+}
+
+if (isset($_SESSION["selected_tracks_ids"])) {
+    unset($_SESSION["selected_tracks_ids"]);
+}
+
 header("Location: index.php");
--- template/notification_templ.php
+++ template/notification_templ.php
@@ -2,28 +2,28 @@
     <header>
         <h1>Notifications</h1>
     </header>
-    <section id="notification_list">
+    <div id="notification_list">
         <?php foreach ($template["notifications"] as $notif): ?>
             <article id="<?php echo $notif["NotificationID"]?>" class="notification">
                 <em class="fa-solid fa-xmark close"></em>
                 <div>
                     <?php switch($notif["Type"]):
-                            case 'Follower': ?>
-                                <h3>New Follower</h3>
-                                <p>The user <a href="profile.php?profile=<?php echo $notif["TriggeringUser"]?>">@<?php echo $notif["TriggeringUser"]?></a> has just started following you!<br/> You're becoming famous.</p>
-                            <?php break; ?>
-                            <?php case 'Post':?>
-                                <h3>New Post</h3>
-                                <p>The user <a href="profile.php?profile=<?php echo $notif["TriggeringUser"]?>">@<?php echo $notif["TriggeringUser"]?></a> has just published an awesome post!</p>
-                                <a href="single_post.php?id=<?php echo $notif["PostID"]?>">Click here to check it out</a>
-                            <?php break;?>
-                            <?php case 'Like':?>
-                                <h3>Like</h3>
-                                <p>The user <a href="profile.php?profile=<?php echo $notif["TriggeringUser"]?>">@<?php echo $notif["TriggeringUser"]?></a> has liked one of your posts!</p>
-                                <a href="single_post.php?id=<?php echo $notif["PostID"]?>">Click here to see the post</a>
-                            <?php break;?>
+                        case 'Follower': ?>
+                            <h2>New Follower</h2>
+                            <p>The user <a href="profile.php?profile=<?php echo $notif["TriggeringUser"]?>">@<?php echo $notif["TriggeringUser"]?></a> has just started following you!<br/> You're becoming famous.</p>
+                        <?php break; ?>
+                        <?php case 'Post':?>
+                            <h2>New Post</h2>
+                            <p>The user <a href="profile.php?profile=<?php echo $notif["TriggeringUser"]?>">@<?php echo $notif["TriggeringUser"]?></a> has just published an awesome post!</p>
+                            <a href="single_post.php?id=<?php echo $notif["PostID"]?>">Click here to check it out</a>
+                        <?php break;?>
+                        <?php case 'Like':?>
+                            <h2>Like</h2>
+                            <p>The user <a href="profile.php?profile=<?php echo $notif["TriggeringUser"]?>">@<?php echo $notif["TriggeringUser"]?></a> has liked one of your posts!</p>
+                            <a href="single_post.php?id=<?php echo $notif["PostID"]?>">Click here to see the post</a>
+                        <?php break;?>
                         <?php case 'Comment':?>
-                            <h3>Comment</h3>
+                            <h2>Comment</h2>
                             <p>The user <a href="profile.php?profile=<?php echo $notif["TriggeringUser"]?>">@<?php echo $notif["TriggeringUser"]?></a> has commented one of your posts!</p>
                             <a href="single_post.php?id=<?php echo $notif["PostID"]?>">Click here to check it out</a>
                         <?php break;?>
@@ -35,5 +35,5 @@
             </article>
         <?php endforeach; ?>
         <script src="js/notification.js"></script>
-    </section>
+    </div>
 </main>
