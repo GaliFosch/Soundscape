409eddfb83e38988f205ea8d9a92dfdbf256fd77
Galileo Foschini <galileo.foschini@studio.unibo.it>
2024-06-30 17:58:07 +0200

    fixed problem with index.js

--- js/index.js
+++ js/index.js
@@ -176,7 +176,6 @@ function addCloseListener() {
     closeComment.addEventListener("click", () => {
         commentFocus.remove();
         commentOpen = false;
-        document.querySelector('body').style.overflow = "scroll";
     });
   }
 };
